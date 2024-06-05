861c2ef608df58ccda9c1ce2f981b9ebb5244ef9
ZaforAbdullah <abuzaforabd@gmail.com>
2024-06-06 00:27:26 +0800

    first commit

---
+++ db/seeds/profiles.rb
@@ -0,0 +1,13 @@
+Profile.create!(
+  user_id: 2,
+  weight: 74.65,
+  height: 173,
+  fitness_goal: 'weight'
+)
+
+Profile.create!(
+  user_id: 3,
+  weight: 67,
+  height: 173,
+  fitness_goal: 'muscle'
+)
